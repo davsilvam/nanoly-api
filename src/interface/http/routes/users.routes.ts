e931061f205530a59158597e46f346400c454ef4
David Silva <contatodavidmaia@gmail.com>
2024-04-29 20:01:20 -0300

    chore: remove unused controller

--- src/interface/http/routes/users.routes.ts
+++ src/interface/http/routes/users.routes.ts
@@ -2,12 +2,10 @@ import type { FastifyInstance } from 'fastify'
 
 import { authenticate } from '../controllers/users/authenticate.controller'
 import { getProfile } from '../controllers/users/get-profile.controller'
-import { refreshToken } from '../controllers/users/refresh-token.controller'
 import { register } from '../controllers/users/register.controller'
 
 export async function usersRoutes(app: FastifyInstance) {
   await register(app, '/users')
   await authenticate(app, '/sessions')
-  await refreshToken(app, '/token/refresh')
   await getProfile(app, '/users/profile')
 }
